d65e63be400214ffef2d0c1ccb2ce628b05fe0bf
Mariano Tahay <marianotahay15@gmail.com>
2023-11-02 19:55:32 -0600

    Imagen usuario implementado

--- app/assets.js
+++ app/assets.js
@@ -1,25 +1,52 @@
 const pool = require('../config/database');
 const multer = require('multer');
+const express = require('express');
+const fs = require('fs');
 const path = require('path');
 
 module.exports = (app) => {
 
-  
+  app.use('/static', express.static('assets'))
+
+  app.post('/getProfilePic', (req, res) => {
+
+    const {name, tipo} = req.body;
+
+    const rutaImagen = "http://localhost:3000/static/" + tipo + "/" + name;
+
+    fs.access('assets/' + tipo + '/' + name, fs.constants.F_OK, (err) => {
+      if(err){
+        res.send("")
+        console.log('assets/' + tipo + '/' + name)
+      }
+      else{
+        res.send(rutaImagen);
+      }
+    })
+
+  })
+
   const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'assets/' + req.body.type + '/')
+
+      const tipo = req.body.tipo
+
+      cb(null, 'assets/' + tipo)
     },
     filename: function (req, file, cb) {
 
-      cb(null, req.body.id + path.extname(file.originalname))
+      const id = req.body.id
+
+      cb(null, id + path.extname(file.originalname))
     }
-  });
+  })
+  
+  const upload = multer({ storage: storage })
 
-  const upload = multer({ storage: storage });
+  app.post('/subirArchivo', upload.single('archivo'), (req, res) => {
 
-  app.post('/subir-archivo', upload.single('archivo'), (req, res) => {
-
-    console.log("./../../../../../../backend/" + req.body.type + "/" + req.file.filename)
+    console.log("Archivo subido");
 
   })
+
 }
